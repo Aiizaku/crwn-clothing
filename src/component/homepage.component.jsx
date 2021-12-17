9fd59618503c53bbf21bfc2fa692c7e4b9409f7e
Isaac Brown <aiizaku@Live.com>
2021-12-17 11:29:59 -0500

    update with the layout for homepage

--- src/component/homepage.component.jsx
+++ src/component/homepage.component.jsx
@@ -9,32 +9,32 @@ const HomePage = () => (
                     <span className='subtitle'>SHOP NOW!</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
+        
+       
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>JACKET</h1>
                     <span className='subtitle'>SHOP NOW!</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
+        
+    
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>SNEAKERS</h1>
                     <span className='subtitle'>SHOP NOW!</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
+        
+       
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>MEN</h1>
                     <span className='subtitle'>SHOP NOW!</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
+        
+      
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>WOMEN</h1>
